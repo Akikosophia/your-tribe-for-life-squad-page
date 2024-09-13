23b37047173a35c83aa97d3fb63ce92c3bfbac2f
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2024-09-13 14:42:04 +0200

    Squadpage design
    Design voor de homepage
    - Foto's
    - Styling

---
+++ src/routes/+page.js
@@ -0,0 +1,3 @@
+export let csr = false
+
+// Dit zorgt ervoor dat je client side uitzet en dat alleen de server side aan staat
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2>Jesse sucks</h2>
-<h3>Tom is nogal gay</h3>
+<body>
+
+		<h1>Meet our team of experts</h1>
+
+		<p>
+			Wij zijn de enthousiaste studenten die de opleiding Front-end Design & Development volgen aan
+			de HvA.
+		</p>
+
+		<div class="grid-container">
+			<img src="/Lesley.JPG" class="studenten" alt="Lesley" />
+			<div class="grid-item">D</div>
+
+			<img src="/Zainab.JPG" class="studenten" alt="" />
+			<div class="grid-item">F</div>
+
+			<img src="/Eva.JPG" class="studenten" alt="" />
+			<div class="grid-item">N</div>
+
+			<img src="/Rukiya.JPG" class="studenten" alt="" />
+			<div class="grid-item">D</div>
+
+			<img src="/Akiko.JPG" class="studenten" alt="Akiko" />
+		</div>
+
+     
+
+</body>
+
+<style>
+
+    p {
+        margin-bottom: 20px;
+    }
+
+	.grid-container {
+		display: grid;
+		grid-template-columns: auto auto auto;
+		background-color: #050542;
+	}
+
+	.grid-item {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid rgba(192, 186, 186, 0.8);
+		padding: 32px;
+		font-size: 32px;
+		text-align: center;
+	}
+
+</style>
